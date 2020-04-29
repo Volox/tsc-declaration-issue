9fc80070a6f52af043c061a874fb8203b2a440d7
Volox <voloxxx@gmail.com>
2020-04-29 14:56:52 +0200

    Working example

---
+++ packages/lib1/src/index.ts
@@ -0,0 +1,6 @@
+
+export type Test = {
+    foo: number
+}
+
+export const bar = 5;
---
+++ packages/lib2/src/index.ts
@@ -0,0 +1,11 @@
+import {bar} from 'lib1'
+
+import type {Test} from 'lib1'
+
+const baz: Test = {
+    foo: 5
+}
+
+// do something...
+console.log(bar);
+console.log(baz);
